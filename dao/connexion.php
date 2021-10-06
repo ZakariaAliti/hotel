3e735a419a4274cea2b081b5fc1a64430def0101
Zakaria Aliti <Zakariaaliti76@gmail.com>
2021-10-06 13:40:23 +0100

    dateBase

--- dao/connexion.php
+++ dao/connexion.php
@@ -2,11 +2,14 @@
 
 // préparation des informations nécessaires 
 
-define("DSN","mysql:host=localhost;dbname=hotel"); 
-define("USER","root");
-define("MDP","MYSQ");
+//define("DSN","mysql:host=localhost;dbname=hotel"); 
+//define("USER","root");
+//define("MDP","MYSQ");
 
- // création d'un objet PDO et connexion 
+define("DSN","mysql:host=sql11.freesqldatabase.com;dbname=sql11442538"); 
+define("USER","sql11442538");
+define("MDP","SkIFqSWYdt");
+ 
 try {
 $conn=new PDO(DSN,USER,MDP);
 } catch (Exception $erreur) {
